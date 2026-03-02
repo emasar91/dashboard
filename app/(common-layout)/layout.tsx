5e422ad94fd52aa3f01983c07f50ab51ebb9bd27
Emanuel Sarco <emasar91@gmail.com>
2026-03-02 20:31:01 -0300

    fix mobile

--- app/(common-layout)/layout.tsx
+++ app/(common-layout)/layout.tsx
@@ -11,7 +11,7 @@ function Layout({ children }: { children: React.ReactNode }) {
     <div className="flex h-dvh overflow-hidden bg-background">
       <Sidebar currentSelected={path.replace("/", "")} />
 
-      <main className="w-full min-h-screen bg-background p-3 md:p-4 lg:p-6 ml-15 sm:ml-0 overflow-y-auto">
+      <main className="w-full min-h-dvh bg-background p-3 md:p-4 lg:p-6 ml-15 sm:ml-0 overflow-y-auto">
         <div className="mb-4 mx-auto w-full max-w-7xl">
           <h1 className="text-xl font-bold text-card-foreground">
             {t("title")}
